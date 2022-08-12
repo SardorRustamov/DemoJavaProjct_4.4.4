40f23c271198a828360ff82d4c3536d73a7d5af6
Sardor <sardor.rustamov.a@gmail.com>
2022-08-12 08:51:30 +0500

    New

--- app/src/main/java/com/example/demojavaprojct/DetailActivity.java
+++ app/src/main/java/com/example/demojavaprojct/DetailActivity.java
@@ -2,7 +2,11 @@ package com.example.demojavaprojct;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -15,7 +19,21 @@ public class DetailActivity extends AppCompatActivity {
     }
     void initViews(){
         TextView tv_detail = findViewById(R.id.tv_detail);
+        Button b_exit= findViewById(R.id.b_exit);
+        b_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Member member = new Member("Object", "Thank you!");
+                backToFinish(member);
+            }
+        });
         User user = (User) getIntent().getSerializableExtra("user");
         tv_detail.setText(user.toString());
+    }
+    void backToFinish(Member member){
+        Intent intent = new Intent();
+        intent.putExtra("result", member);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/demojavaprojct/MainActivity.java
+++ app/src/main/java/com/example/demojavaprojct/MainActivity.java
@@ -1,21 +1,45 @@
 package com.example.demojavaprojct;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int LAUNCH_DETAIL=1001;
+    TextView tv_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
     }
+    ActivityResultLauncher<Intent> detailLauncher= registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode()==Activity.RESULT_OK){
+                        Intent data= result.getData();
+                        Member member = (Member) data.getSerializableExtra("result");
+                        tv_home.setText(member.toString());
+                    }
+                }
+            }
+    );
+
     void initViews(){
+        tv_home= findViewById(R.id.tv_home);
         Button b_open_details = findViewById(R.id.b_detail);
         b_open_details.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,9 +49,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     void openDetailActivity(User user){
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra("user", user);
-        startActivity(intent);
+        detailLauncher.launch(intent);
     }
 }
